2b27aaf21de2ad8ff9fe8caf2e699dcaa782a8a2
Oursel06 <alexoursel6@gmail.com>
2023-03-06 16:40:48 +0100

    Page connexion et header

--- page/header.php
+++ page/header.php
@@ -9,6 +9,8 @@
   <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" rel="stylesheet" />
   <!-- MDB -->
   <link href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.2.0/mdb.min.css" rel="stylesheet" />
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
+  <link rel="stylesheet" href="../style.css">
 
   <title>Tp NO SQL</title>
 
--- page/menu.php
+++ page/menu.php
@@ -1,51 +1,25 @@
-<!-- <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
-        <a class="navbar-brand" href="#">Hidden brand</a>
-        <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
-            <li class="nav-item active">
-                <a class="nav-link" href="index.php">Accueil</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="liste_restaurant.php">Restaurant</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="favori.php">Vos Favori</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="connexion.php">Connexion</a>
-            </li>
-        </ul>
-        <form class="form-inline my-2 my-lg-0">
-            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-        </form>
+<?php include 'header.php'; ?>
+<section class="navigation">
+    <div class="nav-container">
+        <div class="brand">
+            <a href="#!">Logo</a>
+        </div>
+        <nav>
+            <div class="nav-mobile"><a id="nav-toggle" href="#!"><span></span></a></div>
+            <ul class="nav-list">
+                <li>
+                    <a href="#!">Acueil</a>
+                </li>
+                <li>
+                    <a href="#!">Restaurants</a>
+                </li>
+                <li>
+                    <a href="#!">Favoris</a>
+                </li>
+                <li>
+                    <a href="#!">Connexion</a>
+                </li>
+            </ul>
+        </nav>
     </div>
-</nav> -->
-
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-    <a class="navbar-brand" href="#">Navbar</a>
-
-    <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
-        <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
-            <li class="nav-item active">
-                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Link</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link disabled" href="#">Disabled</a>
-            </li>
-        </ul>
-        <form class="form-inline my-2 my-lg-0">
-            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-        </form>
-    </div>
-</nav>
+</section>
