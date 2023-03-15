b9bc2db5e2e36506a361f8ece27998fcdaef31a8
Oursel06 <alexoursel6@gmail.com>
2023-03-15 21:54:06 +0100

    affichage restaurants

--- page/liste_restaurant.php
+++ page/liste_restaurant.php
@@ -2,12 +2,28 @@
 <?php include('menu.php'); ?>
 
 <body>
+    <br />
+    <br />
+    <br />
+
     <?php
     $file = 'restaurants.json';
     $data = file_get_contents($file);
-    $obj = json_decode($data);
-    echo $obj[1]->address->building;
+    $restaurant = json_decode($data);
     ?>
+    <div class="restaurant_container">
+        <?php for ($i = 0; $i < 10; $i++) : ?>
+            <div class="restaurant_cards">
+                <i class="restaurant_id"><?php echo "N°" . $restaurant[$i]->restaurant_id; ?></i>
+                <div class="restaurant_cards_top">
+                    <h3><?php echo $restaurant[$i]->name; ?></h3>
+                    <i><?php echo $restaurant[$i]->cuisine; ?> </i>
+                </div>
+                <p><?php echo $restaurant[$i]->address->building . " " . $restaurant[$i]->address->street . ", " . $restaurant[$i]->address->zipcode . " - " . $restaurant[$i]->borough; ?></p>
+                <button>Voir plus</button>
+            </div>
+        <?php endfor ?>
+    </div>
 </body>
 
 </html>
