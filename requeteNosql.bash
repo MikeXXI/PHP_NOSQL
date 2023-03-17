1de4ae9a52638c6feffd6dfc3291454ab67df886
edwinrizk <114926823+edwinrizk@users.noreply.github.com>
2023-03-17 16:32:32 +0100

    modif requete

--- requeteNosql.bash
+++ requeteNosql.bash
@@ -1,39 +1,41 @@
 
-#list des restrautants
-db.restaurants.find()
+#list des restrautants 
+
+ $cursor = $collection->find();
 
 #liste des  favoris par "user_id"
-db.favoris.find({"user_id":?})
+    $favori = $collection->find([("user_id" => $_SESSION['user_id'])]);
 
-#lsuppresion d'un favori par "user_id" et "restaurant_id"
-db.favoris.deleteOne({"user_id":?,"restaurant_id":?})
+#suppresion d'un favori par "user_id" et "restaurant_id"
+   $favori =  $collection->deleteOne([("user_id" => $_SESSION['user_id']), ("restaurant_id" => new MongoDB\BSON\ObjectI($id))]);
 
-#liste des restaurant trier par ordre alphabeique  croissant 
-db.restaurants.find().sort({"name":1})
+
+#liste des restaurant trier par ordre alphabeique  croissant    
+    $restaurent = $collection->find()->sort(["name" => 1]);
 
 #liste des restaurant trier par ordre alphabeique  décroissant
-db.restaurants.find().sort({"name":-1})
+    $restaurent = $collection->find()->sort(["name" => -1]);
 
 #liste des restaurant trier par restaurant_id croissant
-db.restaurants.find().sort({"restaurant_id":1})
+    $restaurent = $collection->find()->sort(["restaurant_id" => 1]);
 
 #liste des restaurant trier par restaurant_id décroissant
-db.restaurants.find().sort({"restaurant_id":-1})
+    $restaurent = $collection->find()->sort(["restaurant_id" => -1]);
 
 #liste des restaurant trier par ordre d’insertion croissant
-db.restaurants.find().sort({"_id":1})
+    $restaurent = $collection->find()->sort(["_id" => 1]);
 
 #liste des restaurant trier par ordre d’insertion décroissant
-db.restaurants.find().sort({"_id":-1})
+    $restaurent = $collection->find()->sort(["_id" => -1]);
    
 #liste des restaurant trier par  type de cuisine
-db.restaurants.find().sort({"cuisine":1})
+    $restaurent = $collection->find()->sort(["cuisine" => 1]);
 
 #liste des restaurant trier par arrondissement
-db.restaurants.find().sort({"borough":1})
+    $restaurent = $collection->find()->sort(["address.zipcode" => 1]);
 
 #liste des restaurant trier par code postal
-db.restaurants.find().sort({"address.zipcode":1})
+    $restaurent = $collection->find()->sort(["address.zipcode" => 1]);
 
 
 
